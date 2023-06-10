7f564dc0807d0f4671673d06d0348183f4a50dfc
Samuel Gómez Pérez <samgope100@gmail.com>
2023-06-10 21:24:35 +0200

    Mejora intento 2

--- src/cabecera/cabecera.js
+++ src/cabecera/cabecera.js
@@ -7,12 +7,10 @@ import Buscador from '../buscador/buscador';
 
 function Cabecera({user, handleLogout}) {
 
-    const logo = "./imagenes/logo.jpg";
-
     return(
         <nav className="navbar navbar-expand-lg bg-light border-bottom">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/"><img id='logo' src={logo}></img></Link>
+                <Link className="navbar-brand" to="/"><img id='logo' src="./imagenes/logo.jpg"></img></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
